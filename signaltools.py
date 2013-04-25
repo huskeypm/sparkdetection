e1518be0921f93d1009af51cbbd1276fd2972c82
huskeypm <huskeypm@gmail.com>
2013-04-24 17:52:50 -0700

    branch-new

--- signaltools.py
+++ signaltools.py
@@ -33,6 +33,7 @@ def printimg(data,filename):
   h = int((data.shape)[0]/1024. * 16)
   figprops = dict(figsize=(w,h), dpi=100)
   fig1 = p.figure(**figprops)
+  p.subplot(111,aspect='equal')
   p.gray()
   d = np.flipud(data) # for images
   p.pcolormesh(d)
@@ -117,6 +118,7 @@ def ApplyNoiseFloor(psd,energy):
   psd[psd < energy] = energy
 
 # Deconvolves signal by powerspectral density 
+# Substracts datamean from data
 # Option is given to apply window to data to prevent edge (Gibbs) effects
 def whiten(data, datamean,psd="none",window="none"):
   demean = data-datamean
@@ -128,10 +130,10 @@ def whiten(data, datamean,psd="none",window="none"):
     demean = ham*demean
 
   if(psd!="none"):
-   print "Using input PSD"
+   print "Whitening using input PSD"
 
   else:
-   print  "Computing PSD"
+   print  "Computing PSD (with slight smoothing) for whitening "
    psd = CalcPsd(demean)
    psd = smooth(psd,size=2)
 
@@ -187,13 +189,14 @@ def whiten2(m,psd="none",sigma=3):
   return(dmw,psds)
 
 # Computes PSD based on PSD in each frame 
+# Substracts meanImg from images 
 def GetAveragePSD(ims,meanImg,size):
   from congrid import congrid
   avgPSD = np.zeros((size,size))
   n= ims.shape[0]
   for i in range(n):
     (wms,psd) = whiten(ims[i,:,:],meanImg)       
-    # normalize
+    # normalize (Parsevals) 
     psdn= psd/np.prod(np.shape(psd))
     # rescale to size of image
     psdr= congrid(psdn,(size,size),minusone=True)
