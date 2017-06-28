5c04825df4828890abaab26e1c146f267e47f34b
Peter Kekenes-Huskey <pkekeneshuskey@uky.edu>
2017-06-28 10:46:56 -0400

    added

---
+++ mach/hexagon.py
@@ -0,0 +1,61 @@
+"""
+Generates hexagonal close packed vertices
+"""
+import numpy as np 
+class center:
+    def __init__(self,x,y):
+        self.x=x
+        self.y = y
+
+def hex_corner(center, size=1, i=0):
+    angle_deg = 60. * i   + 30.
+    angle_rad = np.pi / 180. * angle_deg
+    return [center.x + size * np.cos(angle_rad),
+                 center.y + size * np.sin(angle_rad)]
+
+
+
+def drawHexagon(origin=center(0,0),i=0,j=0,size=1, wcenter=True):
+    # should go elsewhere
+    coords = []
+    height = size * 2    
+    vertDist = height*3/4.
+    width = np.sqrt(3)/2. * height
+    horzDist = width
+
+    if j%2==1:
+      xoffset = np.sqrt(3)/2 * height/2.
+    else:     
+      xoffset = 0  
+    yoffset = size*np.sin(np.pi/6.) 
+
+        
+    hexCtr = center(origin.x+i*width+xoffset, origin.y+j*(size+yoffset))
+
+    # center
+    if wcenter:
+      coords.append([hexCtr.x,hexCtr.y])
+
+    # edges 
+    for i in range(6):
+      coord = hex_corner(hexCtr,size=size,i=i)
+      coords.append(coord)      
+
+    coords = np.array(coords)#,[7,2]
+    return coords 
+
+def drawHexagonalGrid(xIter,yIter,size=1):
+    
+  origin = center(0.,0.)
+  coords = []        
+  for i in range(xIter): 
+    for j in range(yIter): 
+        coord = drawHexagon(origin,i,j,size)
+        coords.append(coord)
+
+  dim = np.shape(coords)
+  coords = np.reshape(coords,[np.prod(dim)/2.,2])
+  return coords 
+
+
+
