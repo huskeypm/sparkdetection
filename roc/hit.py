e27979ff67e22dea1440bb7c27e718287d3651b2
Peter Kekenes-Huskey <pkekeneshuskey@uky.edu>
2017-08-08 10:24:49 -0400

    added procedure for computiung roc curves

---
+++ roc/hit.py
@@ -0,0 +1,47 @@
+"""
+### Evaluate hit:
+- select points within 3 pixels of max
+- integrate intensity
+- collect annulus around maxima that does not include 'maximum area' and compute mean
+- mask out region in original thresholded image
+- store intensity/annulus_mean in output image
+"""
+import numpy as np 
+
+import sys
+sys.path.append("../mach")
+import util 
+
+# image - original image
+# superThresh - image with -subthreshold' parts set to 0
+# hits - array indicating points that pass
+def EvaluateNextHit(image,superThresh,hits,
+                    innerMargin=7,outerMargin=13,threshold=0,
+                    verbose =True):
+    ## Get top hit by intensity 
+    flat = np.ndarray.flatten(superThresh)
+    sidx = np.argsort( flat )[::-1]
+    top = np.unravel_index(sidx[0], np.shape(superThresh))
+    #print top
+
+    ## Get signal/sidelobe ratio
+    #signalInner,integratedValue,area = util.GetRegion(image,top,margin=7)
+    #signalOuter,integratedValue,area = util.GetRegion(image,top,margin=13)
+    annulus,interior = util.GetAnnulus(
+      image,top,innerMargin=innerMargin,outerMargin=outerMargin)
+    mean,stddev = np.mean(annulus), np.std(annulus)
+    integrated = np.sum(interior)
+    value = integrated / mean
+
+    ## mask image 
+    util.MaskRegion(superThresh,top,outerMargin,value=0)
+
+    ## tag solutions
+    isHit = value > threshold
+    if isHit:
+      util.MaskRegion(hits,top,3,value=1)
+    if verbose:
+      print top, value, isHit
+    return isHit
+
+
