1d3aa76443f1cd41b6af67e60b5e5f24be85598c
huskeypm <huskeypm@gmail.com>
2017-10-14 17:50:44 -0400

    sdf

--- mach/fig3.bash
+++ mach/fig3.bash
@@ -1,6 +1,22 @@
+#### fused EM
 IMG=fusedEM
-convert ${IMG}_fused_0.png -geometry 640x A.png
-convert ${IMG}_fused_30.png -geometry 640x B.png
+TYPE=fused
+convert ${IMG}_${TYPE}_0.png -geometry 640x A.png
+convert ${IMG}_${TYPE}_30.png -geometry 640x B.png
+convert ${IMG}.png -geometry 640x C.png
+
+  montage         A.png \
+                  B.png \
+                  C.png \
+          -geometry '+0+0+0+0>' \
+          -tile 1x3  ${IMG}_merged.png
+          #-pointsize 30        
+
+#### fused EM
+IMG=bulkEM
+TYPE=bulk
+convert ${IMG}_${TYPE}_0.png -geometry 640x A.png
+convert ${IMG}_${TYPE}_30.png -geometry 640x B.png
 convert ${IMG}.png -geometry 640x C.png
 
   montage         A.png \
