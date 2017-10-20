12364fe230150ebb66574d0af56bb1732d10f1fa
Peter Kekenes-Huskey <pkekeneshuskey@uky.edu>
2017-10-19 23:40:36 -0400

    added final pieces

---
+++ mach/figs.bash
@@ -0,0 +1,32 @@
+#### fused EM
+IMG=fusedEM
+TYPE=fused
+convert ${IMG}_${TYPE}_0.0.png -geometry 640x A.png
+convert ${IMG}_${TYPE}_30.0.png -geometry 640x B.png
+convert ${TYPE}MarkedBest.png -geometry 640x C.png
+
+  montage         A.png \
+                  B.png \
+                  C.png \
+          -geometry '+0+0+0+0>' \
+          -tile 1x3  ${IMG}_merged.png
+          #-pointsize 30        
+
+#### fused EM
+IMG=bulkEM
+TYPE=bulk
+convert ${IMG}_${TYPE}_0.0.png -geometry 640x A.png
+convert ${IMG}_${TYPE}_30.0.png -geometry 640x B.png
+convert ${TYPE}MarkedBest.png -geometry 640x C.png
+
+  montage         A.png \
+                  B.png \
+                  C.png \
+          -geometry '+0+0+0+0>' \
+          -tile 1x3  ${IMG}_merged.png
+          #-pointsize 30        
+
+
+# merge Best
+# merge ROC 
+
---
+++ mach/figs.py
@@ -0,0 +1,177 @@
+import cv2
+import numpy as np
+import bankDetect as bD
+import matplotlib.pylab as plt
+
+class empty():pass
+root = "./testimages/"
+rawData = root+ 'clahe_Best.jpg'
+fuzedData = root+ 'fusedCellTEM.png'
+bulkData = root+ 'bulkCellTEM.png'
+scale=1.2
+fusedThresh=0.2
+bulkThresh = 0.46
+sigma_n = 1.  # based on Ryan's data 
+iters = [5,30]
+iters = np.linspace(0,90,10)
+
+def GenFig3(): 
+  TestFused()
+  TestBulk()
+
+import optimizer
+def GenFigROC():
+
+  # coarse/fine
+  #ft = np.concatenate([np.linspace(0.5,0.7,7),np.linspace(0.7,0.95,15)   ])
+  #bt = np.concatenate([np.linspace(0.4,0.55,7),np.linspace(0.55,0.65,15)   ])
+  bt = np.linspace(0.05,0.50,10)
+  ft = np.linspace(0.05,0.30,10)
+  scales = [1.2]  # tried optimizing, but performance seemed to decline quickly far from 1.2 nspace(1.0,1.5,6)  
+  optimizer.Assess(
+        fusedThreshes = ft,
+        bulkThreshes = bt,
+        scales = scales,
+        sigma_n = 1.,
+        useFilterInv=True,
+        hdf5Name = "optimizeinvscale.h5",
+        display=False
+      )
+  optimizer.AnalyzePerformanceData(df,tag='bulk',
+                                 normalize=True, roc=True,outName="bulkROC.png")
+  optimizer.AnalyzePerformanceData(df,tag='fused',
+                                 normalize=True,roc=True,outName="fusedROC.png")
+
+
+
+## fused Pore 
+def TestFused():
+  testCase = empty()
+  testCase.label = "fusedEM"
+  testCase.name = rawData
+  testCase.subsection=[340,440,400,500]
+  testCase.outName = "fusedMarkedBest.png"
+  #daImg = cv2.imread(testCase.name)
+  #daImg = cv2.cvtColor(daImg, cv2.COLOR_BGR2GRAY)
+  #raw = daImg[testCase.subsection[0]:testCase.subsection[1],
+  #            testCase.subsection[2]:testCase.subsection[3]]
+  #imshow(cut)
+  DoTest(testCase,fusedThresh=fusedThresh,bulkThresh=bulkThresh,display=True)
+  Extrapolate(testCase,tag=testCase.label)
+
+## fused Pore 
+def TestBulk():
+  testCase = empty()
+  testCase.label = "bulkEM"
+  testCase.name = rawData
+  testCase.subsection=[250,350,50,150] 
+  testCase.outName="bulkMarkedBest.png"
+  DoTest(testCase,fusedThresh=fusedThresh,bulkThresh=bulkThresh,display=True)
+  Extrapolate(testCase,tag=testCase.label)
+  
+  
+def DoTest(testCase,
+  fusedThresh=0.2,
+  bulkThresh = 0.46,
+  display=False
+  ):
+  fusedPoreResult, bulkPoreResult = bD.TestFilters(
+    testCase.name, # testData
+    fuzedData,                       # fusedfilter Name
+    bulkData,                      # bulkFilter name
+    subsection=testCase.subsection, #[200,400,200,500],   # subsection of testData
+    fusedThresh = fusedThresh,
+    bulkThresh = bulkThresh,
+    label = testCase.label,
+    sigma_n = sigma_n,
+    iters = iters, 
+    useFilterInv=True,
+    scale=scale,
+    display=display,  # to gen images for final copy 
+    colorHitsOutName=testCase.outName      
+   )        
+  testCase.fusedPoreResult = fusedPoreResult
+  testCase.bulkPoreResult = bulkPoreResult
+
+
+def GenFigN():
+  testCase = empty()
+  testCase.label = "totalEM"
+  testCase.name = rawData
+  testCase.subsection=None; #[250,350,50,150] 
+ 
+  vals = np.linspace(0.1,0.9,10)
+  for i,val in enumerate(vals): 
+    testCase.outName="fullBest_%3.1f.png"%val
+    DoTest(testCase,fusedThresh=val*fusedThresh,bulkThresh=val*bulkThresh,display=False)
+  
+import painter
+def Extrapolate(
+  testCase,
+  bulk = 1.,
+  fused = 5.,
+  tag = None
+  ):
+  daImg = cv2.imread(testCase.name)
+  daImg = cv2.cvtColor(daImg, cv2.COLOR_BGR2GRAY)
+  raw = daImg[testCase.subsection[0]:testCase.subsection[1],
+              testCase.subsection[2]:testCase.subsection[3]]
+    
+    
+  fusedPoreResult = testCase.fusedPoreResult
+  bulkPoreResult = testCase.bulkPoreResult
+  fusedPoreResult.labeled = painter.doLabel(fusedPoreResult)
+  plt.figure()
+  bulkPoreResult.labeled = painter.doLabel(bulkPoreResult)
+    
+
+  unk = np.mean([bulk,fused])
+
+  # create map, assume everthing that isn't characterized is 'unk' 
+  # with a permeation somewhere between bulk and fused
+  permeationMap  = unk * np.ones_like(raw)
+  totArea =  np.prod(np.shape(permeationMap))*1.  
+
+  # add bulk 
+  isTrue = np.where(bulkPoreResult.labeled)  
+  permeationMap[ isTrue  ] = bulk
+  #print np.shape( isTrue)
+  areaBulk = np.shape( isTrue )[1]/totArea
+  
+    
+    # add fused
+  isTrue = np.where(fusedPoreResult.labeled)  
+  permeationMap[ isTrue  ] = fused
+  areaFused = np.shape( isTrue )[1]/totArea
+
+  plt.subplot(1,2,1)
+  plt.imshow(raw,cmap='gray')
+  plt.title("Raw")
+
+  fig=plt.subplot(1,2,2)
+  fig.set_aspect('equal')
+  #plt.pcolormesh(np.flipud(permeationMap),cmap='gray')
+  plt.imshow(permeationMap,cmap='gray')
+ 
+  areaUnk = 1. - areaBulk - areaFused
+  if tag!=None:
+    print tag
+  print "Bulk, Fused,Unk"
+  print areaBulk, areaFused, areaUnk
+  perm = areaBulk*bulk + areaFused*fused + areaUnk*unk
+  print "eff perm", perm
+    
+  #plt.figure()
+  #plt.axes().set_aspect('equal', 'datalim')
+  #plt.pcolormesh(np.flipud(permeationMap),shading='gourade',cmap='gray')
+  plt.title("Permeation")
+  plt.gcf().savefig("extrapolated.png",dpi=300)
+
+
+
+
+  
+#needs work GenFigN()
+GenFig3()
+#GenFigROC()
+  
